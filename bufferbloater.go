f9e82667502b046f67eafef41b1e7b8bacb2aa4d
ssuman <ssuman@twilio.com>
2021-08-10 06:41:26 -0700

    adding command line argument

--- bufferbloater.go
+++ bufferbloater.go
@@ -1,162 +1,75 @@
 package main
 
 import (
-	"io/ioutil"
+	"strconv"
+	"strings"
 	"sync"
 	"time"
 
 	"go.uber.org/zap"
-	"gopkg.in/yaml.v2"
 
-	"github.com/tonya11en/bufferbloater/client"
 	"github.com/tonya11en/bufferbloater/server"
 	"github.com/tonya11en/bufferbloater/stats"
 )
 
 type Bufferbloater struct {
 	log      *zap.SugaredLogger
-	c        *client.Client
 	s        *server.Server
 	statsMgr *stats.StatsMgr
 }
 
-// Basic representation of the parsed yaml file before the durations are parsed.
-type parsedYamlConfig struct {
-	Client struct {
-		Workload []struct {
-			Rps      uint
-			Duration string
-		} `yaml:"workload"`
-		RqTimeout    string `yaml:"rq_timeout"`
-		TargetServer struct {
-			Address string
-			Port    uint
-		} `yaml:"target_server"`
-	} `yaml:"client"`
-
-	Server struct {
-		Profile []struct {
-			LatencyDistribution []struct {
-				Weight  uint   `yaml:"weight"`
-				Latency string `yaml:"latency"`
-			} `yaml:"latency_distribution"`
-			Duration string
-		} `yaml:"profile"`
-		ListenPort uint `yaml:"listen_port"`
-		Threads    uint `yaml:"threads"`
-	}
-}
-
-// Creates a properly typed client config.
-func clientConfigParse(parsedConfig parsedYamlConfig) (client.Config, error) {
-	// TODO: validate config
-
-	clientConfig := client.Config{
-		TargetServer: client.Target{
-			Address: parsedConfig.Client.TargetServer.Address,
-			Port:    parsedConfig.Client.TargetServer.Port,
-		},
-	}
-
-	d, err := time.ParseDuration(parsedConfig.Client.RqTimeout)
-	if err != nil {
-		return client.Config{}, err
-	}
-	clientConfig.RequestTimeout = d
-
-	for _, stage := range parsedConfig.Client.Workload {
-		d, err := time.ParseDuration(stage.Duration)
-		if err != nil {
-			return client.Config{}, err
-		}
-
-		workloadStage := client.WorkloadStage{
-			RPS:      stage.Rps,
-			Duration: d,
-		}
-		clientConfig.Workload = append(clientConfig.Workload, workloadStage)
-	}
-
-	return clientConfig, nil
-}
-
-func serverConfigParse(parsedConfig parsedYamlConfig) (server.Config, error) {
+func serverConfigParse(latency string, port int, thread int, duration string) (server.Config, error) {
 	// TODO: validate config
 
 	serverConfig := server.Config{
-		ListenPort: parsedConfig.Server.ListenPort,
-		Threads:    parsedConfig.Server.Threads,
+		ListenPort: uint(port),
+		Threads:    uint(thread),
 	}
 
-	for _, segment := range parsedConfig.Server.Profile {
-		s := server.LatencySegment{}
+	s := server.LatencySegment{}
+	// Calculate the latency distribution.
+	s.WeightSum = 0
+	s.LatencyDistribution = []server.WeightedLatency{}
 
-		// Calculate the latency distribution.
-		s.WeightSum = 0
-		s.LatencyDistribution = []server.WeightedLatency{}
-		for _, wl := range segment.LatencyDistribution {
-			l, err := time.ParseDuration(wl.Latency)
-			if err != nil {
-				return server.Config{}, err
-			}
+	latencyInput := strings.Split(latency, ",")
+	for _, stage := range latencyInput {
+		weights := strings.Split(stage, ":")
 
-			weightedLatency := server.WeightedLatency{
-				Weight:  wl.Weight,
-				Latency: l,
-			}
-			s.LatencyDistribution = append(s.LatencyDistribution, weightedLatency)
-			s.WeightSum += wl.Weight
-		}
-
-		d, err := time.ParseDuration(segment.Duration)
+		weight, err := strconv.Atoi(weights[0])
 		if err != nil {
 			return server.Config{}, err
 		}
-		s.SegmentDuration = d
+		latencyDuration, err := time.ParseDuration(weights[1])
+		if err != nil {
+			return server.Config{}, err
+		}
 
-		serverConfig.Profile = append(serverConfig.Profile, s)
+		weightedLatency := server.WeightedLatency{
+			Weight:  uint(weight),
+			Latency: latencyDuration,
+		}
+		s.WeightSum += uint(weight)
+		s.LatencyDistribution = append(s.LatencyDistribution, weightedLatency)
 	}
+
+	d, err := time.ParseDuration(duration)
+	if err != nil {
+		return server.Config{}, err
+	}
+	s.SegmentDuration = d
+
+	serverConfig.Profile = append(serverConfig.Profile, s)
 
 	return serverConfig, nil
 }
 
-func parseConfigFromFile(configFilename string) (parsedYamlConfig, error) {
-	// Read the config file.
-	data, err := ioutil.ReadFile(configFilename)
-	if err != nil {
-		return parsedYamlConfig{}, err
-	}
-
-	// Parse the config file yaml.
-	var parsedConfig parsedYamlConfig
-	err = yaml.UnmarshalStrict([]byte(data), &parsedConfig)
-	if err != nil {
-		return parsedYamlConfig{}, err
-	}
-
-	return parsedConfig, nil
-}
-
-func NewBufferbloater(configFilename string, logger *zap.SugaredLogger) (*Bufferbloater, error) {
+func NewBufferbloater(latency string, port int, thread int, duration string, logger *zap.SugaredLogger) (*Bufferbloater, error) {
 	bb := Bufferbloater{
 		log:      logger,
 		statsMgr: stats.NewStatsMgrImpl(logger),
 	}
 
-	parsedConfig, err := parseConfigFromFile(configFilename)
-	if err != nil {
-		bb.log.Fatalw("failed to parse yaml file",
-			"error", err)
-	}
-
-	clientConfig, err := clientConfigParse(parsedConfig)
-	if err != nil {
-		bb.log.Fatalw("failed to create server config",
-			"error", err)
-	}
-	bb.c = client.NewClient(clientConfig, logger, bb.statsMgr)
-
-	serverConfig, err := serverConfigParse(parsedConfig)
+	serverConfig, err := serverConfigParse(latency, port, thread, duration)
 	if err != nil {
 		bb.log.Fatalw("failed to create server config",
 			"error", err)
@@ -167,20 +80,9 @@ func NewBufferbloater(configFilename string, logger *zap.SugaredLogger) (*Buffer
 }
 
 func (bb *Bufferbloater) Run() {
-	// TODO: make folder configurable.
-	defer bb.statsMgr.DumpStatsToFolder("data")
-
-	stopStats := make(chan struct{}, 1)
-	var statsWg sync.WaitGroup
-	statsWg.Add(1)
-	go bb.statsMgr.PeriodicStatsCollection(100*time.Millisecond, stopStats, &statsWg)
 
 	var wg sync.WaitGroup
 	wg.Add(1)
 	go bb.s.Start(&wg)
-	//go bb.c.Start(&wg)
 	wg.Wait()
-
-	stopStats <- struct{}{}
-	statsWg.Wait()
 }
--- main.go
+++ main.go
@@ -1,24 +1,24 @@
 package main
 
 import (
-	"os"
+	"flag"
 
 	"go.uber.org/zap"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		panic("failed to provide a config file")
-	}
-	configFile := os.Args[1]
-
+	latency := flag.String("latency", "90:50ms,5:60ms,4:100ms,1:150ms", "RPS1:duration1,RPS2:duration...")
+	port := flag.Int("port", 9002, "Request timeout duration in sec")
+	thread := flag.Int("thread", 8, "ServerAddress:Port")
+	duration := flag.String("duration", "7200s", "duration for the server up")
+	flag.Parse()
 	logger, err := zap.NewDevelopment()
 	if err != nil {
 		panic("couldn't initialize logging")
 	}
 	sugar := logger.Sugar()
 
-	bb, err := NewBufferbloater(configFile, sugar)
+	bb, err := NewBufferbloater(*latency, *port, *thread, *duration, sugar)
 	if err != nil {
 		sugar.Fatalw("failed to create bufferbloater",
 			"error", err)
